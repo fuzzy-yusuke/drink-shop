88db28a74424ad7722eb5260f207b0a3ea5d7605
fuzzy-yusuke <youth_k0228@outlook.com>
2021-06-01 13:59:04 +0900

    テーブル構築完了

---
+++ database/migrations/2021_06_01_044051_create_item_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('class');
+            $table->string('maker');
+            $table->string('content');
+            $table->string('quantity');
+            $table->string('picture');
+            $table->string('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item');
+    }
+}
