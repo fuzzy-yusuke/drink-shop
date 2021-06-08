c58c4e62e0e54c22dcf07e57964a7ad85cd82bb2
fuzzy-yusuke <youth_k0228@outlook.com>
2021-06-08 18:27:18 +0900

    元に戻した

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Item;
 use App\User;
+use \InterventionImage;
 
 class ItemController extends Controller
 {
@@ -13,6 +14,11 @@ class ItemController extends Controller
     {
         //itemテーブルに格納されているデータを一覧表示する
         $items = Item::all();
+        /*InterventionImage::make($items->picture)
+        ->resize(300,300,function($constraint){
+            $constraint->aspectRatio();
+        })
+        ->save(picture('/picture/' . $picture));*/
         return view ('index',['items'=>$items]);
     }
     
