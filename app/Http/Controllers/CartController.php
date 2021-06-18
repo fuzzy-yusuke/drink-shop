ff2e45fbd3230cc85e32497b4430cfc8aac204c7
fuzzy-yusuke <youth_k0228@outlook.com>
2021-06-18 17:25:08 +0900

    買い物かご機能 コメント追加

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -18,13 +18,17 @@ class CartController extends Controller
     public function addCart(Request $request){
         $user_id=Auth::id();
         //POST送信で送られた商品データを取得
+        $cart=new Cart;
         $item_id=$request->item_id;
+        $cart->quantity=request('quantity');
+        $message='買い物かごに追加しました';
         //同じ商品IDとユーザーIDが無いか確認
         $add_cart=Cart::firstOrCreate(['item_id'=>$item_id,'user_id'=>$user_id]);
 
         if($add_cart->wasRecentlyCreated){
-            $message='買い物かごに追加しました';
+            //既にある商品の個数に、リクエスト送信された個数を足してDBに格納
         }else{
+            //DBに新規登録
             $message='買い物かごに登録済みです';
         }
         $carts=Cart::where('user_id',$user_id)->get();
