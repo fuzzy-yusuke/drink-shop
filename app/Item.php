5073e48257c15930c69202457783451dab227b9a
fuzzy-yusuke <youth_k0228@outlook.com>
2021-07-27 13:38:30 +0900

    ページネーション実装完了

--- app/Item.php
+++ app/Item.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
@@ -13,4 +14,11 @@ class Item extends Model
     public function user(){
         return $this->belongsTo('App\User');
     }
+
+    //メーカー絞り込み
+    public function scopeSearch(Builder $query,array $params):Builder
+    {
+    if(!empty($params['maker'])) $query->where('maker',$params['maker']);
+    return $query;
+}
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,14 +2,18 @@
 @section('content')
 <div class="card-inner inner">
     <h2 class="page-title">商品一覧</h2>
-
-
+    <form>
+        <select class="form-control" name="company">
+            <option selected="selected" value=""></option>
+        </select>
+    </form>
+    {{$items -> links()}}
     <div class="card-container">
         <div class="row">
             @push('css')
             <link href="{{ asset('css/index.css')}}" rel="stylesheet">
             @endpush
-            
+
             @foreach($items as $item)
             <div class="card-item">
                 <a href="{{ route('item.show',['id' => $item -> id])}}">
@@ -21,7 +25,7 @@
             </div>
             </a>
             @endforeach
-            {{$items -> links()}}
+
         </div>
     </div>
 </div>
