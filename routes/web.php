f1ba9e17aa002fdacd4a56b2c8eeeff489f1f48a
fuzzy-yusuke <youth_k0228@outlook.com>
2021-07-30 14:56:34 +0900

    コメント追加

--- routes/web.php
+++ routes/web.php
@@ -25,17 +25,28 @@ Route::get('/login', function () {
 
 Route::group(['middleware'=>['auth','verified']],function(){
 
+//トップ画面
 Route::get('/user/top','UserController@top')->name('user.top');
 
+//一覧画面
 Route::get('/index','ItemController@index')->name('item.index');
 Route::get('/search','ItemController@index')->name('item.search');
 
+//詳細画面
 Route::get('/show/{id}','ItemController@show')->name('item.show');
 
+//購入画面
 Route::get('/pay/{id}','ItemController@pay')->name('item.pay');
 Route::post('/buy/{id}','ItemController@buy')->name('item.buy');
 
+//追加画面
 Route::get('/new','ItemController@new')->name('item.new');
+
+//追加完了画面
+Route::post('/item','ItemController@store')->name('item.store');
+
+//追加完了画面
+Route::get('/complete/{id}','ItemController@complete')->name('item.complete');
 
 
 });
